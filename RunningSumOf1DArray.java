25a5a944e7ed38d44377f98a84ba3aa28fb374bc
Suhaas D <suhasdgupta55555@gmail.com>
2021-05-03 23:25:55 +0530

    Create RunningSumOf1DArray.java

---
+++ RunningSumOf1DArray.java
@@ -0,0 +1,13 @@
+class RunningSumOf1DArray {
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        for(int i=0;i<res.length;i++){
+            if(i==0){
+                res[i] = nums[i];
+            }else{
+                res[i] = res[i-1] + nums[i];
+            }
+        }
+        return res;
+    }
+}
